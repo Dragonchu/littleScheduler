258c63634a730be4afca5520a51d2129a49d3836
Dragonchu <43578886+Dragonchu@users.noreply.github.com>
2020-10-18 16:55:35 +0800

    Add files via upload
    第一次上传，程序在多处理器运行时仍然有bug，代码写的冗杂，结构不清晰，注释太少

---
+++ scheduler/src/scheduler/ScheduleCore.java
@@ -0,0 +1,11 @@
+package scheduler;
+import java.util.Vector;
+
+
+public class ScheduleCore {
+
+	public static void main(String[] args) {
+		new Schedule_Frame();
+		
+	}
+}
---
+++ scheduler/src/scheduler/partiontable.java
@@ -0,0 +1,39 @@
+package scheduler;
+//内存分区表
+public class partiontable {
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int getStartaddress() {
+		return startaddress;
+	}
+	public void setStartaddress(int startaddress) {
+		this.startaddress = startaddress;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public partiontable(int number,int size,int startaddress,int state) {
+		this.number = number;
+		this.size = size;
+		this.startaddress = startaddress;
+		this.state = state;
+	}
+	private int number;
+	private int size;
+	private int startaddress;
+	private int state;
+	
+}
